d1501455a0677ab521efddf18eb0fc838ce7995f
Esra Kayhan <esrakayhan91@gmail.com>
2026-02-25 21:20:01 +0200

    first hw

---
+++ sprint 1/sprint1.js
@@ -0,0 +1,86 @@
+var catName = "Mauro";
+let age = 2;
+let favColor = "purple";
+
+console.log(catName);
+console.log(age);
+console.log(favColor);
+
+//exercise 2
+let x = 5
+let y = 10
+
+console.log(x)
+console.log(y)
+
+x = 10
+y = 5
+
+console.log(x)
+console.log(y)
+
+//exercise 3
+let num1 = 74;
+let num2 = 102;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let division = num1 / num2;
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(division)
+
+//exercise 4
+let price = 15;
+let quantity = 4;
+let totalcost = price * quantity
+console.log(totalcost)
+
+//exercise 5
+let testNum = 48
+let result = 48 %2
+console.log(result)
+
+//Mini Project(Sprint Challenge)
+
+// Simple Calculator
+var n1 = 2
+var n2 = 15
+
+sum= n1 + n2
+difference = n1 - n2
+product = n1 * n2
+division = n1 / n2
+remainder = n1 % n2
+
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(division)
+console.log(remainder)
+
+// Bonus Challenges
+let sec = 60
+let min = 4
+result= sec * min
+console.log(result)
+
+var hour = 9
+var minInhour = 60
+result = hour * minInhour
+console.log(result)
+
+var s1 = 647
+var s2 = 857
+var s3 = 284
+var avg = (s1 + s2+ s3) / 3
+console.log(avg)
+
+//Stretch Goals
+let friend1 = Number(prompt("type a number or the computer explodes! :D"))
+let friend2 = Number(prompt("one more, don't be shy :)"))
+
+let res = friend1 + friend2
+console.log("Beep boop! Math is hard, but I did it for you: " + res)
+alert("Beep boop! Math is hard, but I did it for you: " + res)
